34985c1fc6b7b4a9c025bab376b412d2670c3b60
WinnFreeza27 <erwinpratameyr7@gmail.coom>
2024-11-13 22:06:43 +0800

    dashboard pages bp

--- app/page.js
+++ app/page.js
@@ -1,101 +1,57 @@
+"use client"
+
 import Image from "next/image";
+import {useState} from "react";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <>
+    <div className="fixed top-10 right-5 z-50 p- md:hidden cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+      {
+        isOpen ? (
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+          </svg>
+        ) : (
+          <svg xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        )
+      }
     </div>
+    <nav className="font-roboto flex justify-between p-5 items-center text-accentdarken bg-white mb-20 transition-all">
+      <div className="flex gap-5 items-center">
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={50}
+          height={50}
+        />
+        <div className="font-bold max-w-[20ch]">DINAS KESEHATAN KOTA BANJARBARU</div>
+      </div>
+      <ul className={`${isOpen ? "fixed w-full h-screen top-0 left-0 bg-white flex flex-col justify-center items-center" : "hidden"} md:relative md:flex-row gap-5 md:w-max md:h-auto`}>
+        <li><a className="cursor-pointer hover:underline">Website Resmi</a></li>
+        <li><a className="cursor-pointer hover:underline">Hubungi Kami</a></li>
+      </ul>
+    </nav>
+
+    <div className="flex flex-col md:grid grid-cols-2 justify-center justify-items-center items-center bg-white font-roboto text-accentdarken p-10">
+      <div className="max-w-[50ch] flex flex-col gap-5">
+        <h1 className="font-extrabold text-7xl sm:text-8xl">SICANDI</h1>
+        <p className="font-light text-primary">Mudah, cepat, dan aman – optimalkan manajemen pencatatan data pada instansi Anda dengan Sicandi, aplikasi berbasis digital yang dirancang untuk mendukung  pencatatan modern dan efisien.</p>
+        <button className="justify-self-start w-full sm:w-max px-10 py-3 bg-accent text-white rounded-md hover:bg-accentdarken transition-all">Dashboard</button>
+      </div>
+      <div>
+        <Image
+          src="/dashboard.png"
+          alt="dashboard"
+          width={400}
+          height={400}
+        />
+      </div>
+    </div>
+
+    </>
   );
 }
